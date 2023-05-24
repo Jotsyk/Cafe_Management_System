83bce2a8f0b8a3760d3fd52950ccc3c7dffd950f
Jyoti Kumari <43550667+Jotsyk@users.noreply.github.com>
2023-05-24 17:55:41 +0530

    Update table.sql

--- Backend-Cafe-NodeJs/table.sql
+++ Backend-Cafe-NodeJs/table.sql
@@ -18,3 +18,14 @@ CREATE TABLE [dbo].[category](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
+
+CREATE TABLE [dbo].[Product](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [varchar](255) NOT NULL,
+	categoryId [int] NOT NULL,
+	description [varchar](255),
+	price [int],
+	status [varchar](20)
+	)
+ ON [PRIMARY]
+GO
